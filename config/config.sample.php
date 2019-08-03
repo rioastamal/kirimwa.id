d866fe100a1fe2e5b7fca0e5e39884ed730ac5c9
Rio Astamal <me@rioastamal.net>
2019-08-03 10:35:48 +0700

    Fix wrong ignore file. It should be config/config.php instead of the sample file

---
+++ config/config.sample.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Configuration for KirimWA.id
+ */
+return [
+    // Indonesia country code is 62
+    'countryCode' => '62',
+
+    // Handler or alias for each URL. e.g https://domain/fancy-url
+    // "/fancy-url" is the array key
+    'handler' => [
+        '/xstore' => [
+            // International phone number format
+            // e.g:
+            'phone' => '62812012345689',
+
+            // Text to be displayed at chat
+            'text' => <<<TEXT
+Hello X-Store,
+I want to order.
+TEXT
+,
+            // false => Value of text above is not encoded, so you have encode
+            //          it first before sending to WhatsApp protocol.
+            // true => Value of text above is encoded, it will be send as
+            //         it is.
+            'encoded' => false,
+        ]
+    ]
+];
